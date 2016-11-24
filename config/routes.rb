191909255c48d7830d8953faa7f11bf52856a2a9
h3xar0n <aron@glambyadelina.com>
2016-11-23 22:07:15 -0500

    4.3 Controllers
    Made a landing_page, changed root in routes.rb; it displays the first
    product.
    Made orders controller with blank index and show views.
    Fixed footer in CSS.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :products
-  root 'static_pages#index'
+  resources :orders, only: [:index, :show, :create, :destroy]
+  root 'static_pages#landing_page'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
