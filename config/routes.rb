270548269ad5d5c9aa5d6ed9cc38dbdc364d60db
h3xar0n <aron@glambyadelina.com>
2017-01-20 11:40:41 -0500

    5.1 Routes
    I rerouted my root page to the index page, although I'll probably end
    up placing the carousel at the top of that page further down the road.
    
    For "redirect", I made it so that a successful item submission goes to
    the carousel 'landing_page', so they can see the item in the carousel.
    
    I created links for the nav menu, using '_path' in each case since I
    might change the URLs of the pages in the future.
    
    I also used a dynamic link in the carousel using '<%= link_to
    product.name, product %>' so that a user can visit the product's page
    by clicking on the name when they see it in the carousel.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   resources :products
   resources :orders, only: [:index, :show, :create, :destroy]
-  root 'static_pages#landing_page'
+  root 'static_pages#index'
+  controller :static_pages do
+	  get :landing_page
+	  get :contact
+	end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
