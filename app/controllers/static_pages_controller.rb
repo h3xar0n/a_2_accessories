44b188119dd554a17306af30c6266879d0017bef
h3xar0n <aron@glambyadelina.com>
2017-01-21 10:37:47 -0500

    5.3 Sending Emails (Mailers)
    Added an email form with appropriate controller and connected to
    SendGrid on Heroku.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,4 +2,13 @@ class StaticPagesController < ApplicationController
   def landing_page
     @products = Product.all
   end
+  def thank_you
+    @name = params[:name]
+    @email = params[:email]
+    @message = params[:message]
+    ActionMailer::Base.mail(:from => @email,
+      :to => 'aron.eidelman@gmail.com',
+      :subject => "A2 Accessories form from #{@name}",
+      :body => @message).deliver_now
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
 	  get :landing_page
 	  get :contact
 	end
+	post 'static_pages/thank_you'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
