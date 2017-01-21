2ff07fa93878ce2d96771951602ce258f5937dbf
h3xar0n <aron@glambyadelina.com>
2017-01-21 10:46:57 -0500

    5.3 Changed credentials for mailer

--- config/environment.rb
+++ config/environment.rb
@@ -9,8 +9,8 @@ ActionMailer::Base.smtp_settings = {
   :address => 'smtp.sendgrid.net',
   :port => '587',
   :authentication => :plain,
-  :user_name => ENV['SENDGRID_USERNAME'],
-  :password => ENV['SENDGRID_PASSWORD'],
+  :user_name => ENV['app59142913@heroku.com'],
+  :password => ENV['eniym7zf3690'],
   :domain => 'heroku.com',
   :enable_starttls_auto => true
 }
