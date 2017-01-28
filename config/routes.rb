1f70f182049ff89e80e1597106dfd7af0fc8d819
h3xar0n <aron@glambyadelina.com>
2017-01-28 00:11:08 -0500

    5.8 Comments Controller and Views
    + Major stylistic changes to clear columns
    + Created the controller and views for the product reviews
    + Implement star rating with Raty jQuery plugin, rather than looping in
    Ruby
    + Associated images for Raty

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}
+  resources :products do
+    resources :comments
+  end
   resources :users
-  resources :products
   resources :orders, only: [:index, :show, :create, :destroy]
   root 'static_pages#index'
   controller :static_pages do
