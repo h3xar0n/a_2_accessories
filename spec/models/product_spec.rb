6f4b16cd54c7b7f761fabb10b2b9e8659eaef249
h3xar0n <aron@glambyadelina.com>
2017-02-02 17:42:08 -0500

    6.2 Unit Test
    + Created spec files for each model
    + For Product model test spec/models/product_spec.rb :
    ++ Wrote a passing test for average_rating method
    ++ Wrote a validation test

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe Product do 
+	context "when the product has comments" do
+		before do
+			@product = Product.create!(name: "imperial-military")
+			@user = User.create!(email: "darthvader@galacticempire.com", password: "$ithLord66")
+			@product.comments.create!(rating: 1, user: @user, body: "Apology accepted, Captain Needa.")
+			@product.comments.create!(rating: 3, user: @user, body: "Take care not to choke on your aspirations, Director.")
+			@product.comments.create!(rating: 5, user: @user, body: "You are in command now, Admiral Piett.")
+		end
+
+		it "returns the average rating of all comments" do
+			expect(@product.average_rating).to eq 3
+		end
+
+		it "is not valid" do
+			expect(Product.new(description: "Terrible job and very, very weak. Sad.")).not_to be_valid
+		end
+	end
+end
