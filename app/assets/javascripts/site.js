28e28057e164c7b9fb042ed5ddfe06f9cf4e5383
h3xar0n <aron@glambyadelina.com>
2017-02-09 20:05:25 -0500

    6.10 AJAX
    Used AJAX to create comments remotely
    Used AJAX to render a partial

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,10 +1,14 @@
-$(document).on('turbolinks:load', function(){
-  $('.img-zoom').elevateZoom();
-  $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
-  $('.rated').raty({ path: '/assets', 
-  	readOnly: true, 
-  	score: function() {
-    	return $(this).attr('data-score');
-    }
-  });
+var refreshRating = function() {
+	$('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
+	$('.rated').raty({ path: '/assets', 
+		readOnly: true, 
+		score: function() {
+	  	return $(this).attr('data-score');
+	  }
+	});
+};
+
+$(document).on('turbolinks:load ajaxSuccess', function(){
+  refreshRating();
+  $('.img-zoom').elevateZoom();  
 });
