5e952e01e3402b0fedbacf24cd0b6bb0c869df87
h3xar0n <aron@glambyadelina.com>
2017-02-15 20:31:12 -0500

    6.13 Real-Time Updates with ActionCable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,4 +92,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.web_socket_server_url = "wss://eidelbikes.herokuapp.com/cable"
+config.action_cable.allowed_request_origins = ['https://eidelbikes.herokuapp.com', 'http://eidelbikes.herokuapp.com']
 end
