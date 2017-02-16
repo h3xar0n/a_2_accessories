205623ef1f7fda6615bee387e3c3e156292fd4cf
h3xar0n <aron@glambyadelina.com>
2017-02-16 15:23:01 -0500

    6.14.3 Bug with ActionCable (Do NOT Deploy)

--- app/assets/javascripts/channels/product.js
+++ app/assets/javascripts/channels/product.js
@@ -1,7 +1,5 @@
 App.product = App.cable.subscriptions.create("ProductChannel", {
   connected: function() {
-    console.log('connected');
-    this.listen_to_comments();
     // Called when the subscription is ready for use on the server
   },
 
@@ -11,18 +9,19 @@ App.product = App.cable.subscriptions.create("ProductChannel", {
 
   received: function(data) {
     // Called when there's incoming data on the websocket for this channel
-  $(".alert.alert-info").show();
-  $('.product_reviews').prepend(data.comment);
-  $("#average-rating").attr('data-score', data.average_rating);
-  refreshRating();
-},
+    console.log(data);
+  	$(".alert.alert-info").show();
+  	$('.product-reviews').prepend(data.comment);
+  	$("#average-rating").attr('data-score', data.average_rating);
+  	refreshRating();
+	},
   
 
   listen_to_comments: function () {
     return this.perform('listen', {
       product_id: $("[data-product-id]").data("product-id")
     });
-}
+	}
 
 });
 
