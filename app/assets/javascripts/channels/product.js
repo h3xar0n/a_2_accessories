27f1a955981bb0ab5e00fcb6f5d9b4ad8475a6d3
h3xar0n <aron@glambyadelina.com>
2017-02-16 17:07:45 -0500

    6.14 Using Arguments to Display Actual Comment

--- app/assets/javascripts/channels/product.js
+++ app/assets/javascripts/channels/product.js
@@ -7,9 +7,13 @@ App.product = App.cable.subscriptions.create("ProductChannel", {
     // Called when the subscription has been terminated by the server
   },
 
-  received: function() {
+  received: function(data) {
     // Called when there's incoming data on the websocket for this channel
+    // console.log(data);
     $(".alert.alert-info").show();
+    $('.product-reviews').prepend(data.comment);
+    $("#average-rating").attr('data-score', data.average_rating);
+    refreshRating();
   },
 
   listen_to_comments: function() {
