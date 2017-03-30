46a4b2ab12759a80853d36d965645ee653872198
h3xar0n <aron@glambyadelina.com>
2017-03-30 07:44:39 -0400

    Style guide crosscheck

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,27 +1,27 @@
 class UserMailer < ApplicationMailer
-	default from: 'aron.eidelman@gmail.com'
+  default from: 'aron.eidelman@gmail.com'
 
-	def contact_form(email, name, message)
-	  @message = message
-	    mail(:from => email,
-	        :to => 'your-email@example.com',
-	        :subject => "A new contact form message from #{name}")
-	end
+  def contact_form(email, name, message)
+    @message = message
+      mail(:from => email,
+          :to => 'your-email@example.com',
+          :subject => "A new contact form message from #{name}")
+  end
 
-	def welcome(user)
-	  @appname = "A Squared Accessories"
-	  mail( :to => user.email,
-	        :subject => "Welcome to #{@appname}!")
-	end
+  def welcome(user)
+    @appname = "A Squared Accessories"
+    mail( :to => user.email,
+          :subject => "Welcome to #{@appname}!")
+  end
 
-	def thank_you
-	  @name = params[:name]
-	  @email = params[:email]
-	  @message = params[:message]
-	  UserMailer.contact_form(@email, @name, @message).deliver_now
-	end
+  def thank_you
+    @name = params[:name]
+    @email = params[:email]
+    @message = params[:message]
+    UserMailer.contact_form(@email, @name, @message).deliver_now
+  end
 
-	def paid_success(user, product)
+  def paid_success(user, product)
     @user = user
     @product = product
     mail( :from => 'aron.eidelman@gmail.com',
--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,11 @@ Rails.application.routes.draw do
   resources :orders, only: [:index, :show, :create, :destroy]
   root 'static_pages#landing_page'
   controller :static_pages do
-	  get :landing_page
-	  get :contact
+    get :landing_page
+    get :contact
     get :about
-	end
-	post 'static_pages/thank_you'
+  end
+  post 'static_pages/thank_you'
   post 'payments/create'
   mount ActionCable.server => '/cable'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
