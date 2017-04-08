9ad29a3b6a36d150de5cb52568539f43a5aa0fd5
h3xar0n <aron@glambyadelina.com>
2017-04-08 10:25:29 -0400

    Added test coverage to static pages for contact page, cleaned up indentation

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -27,4 +27,15 @@ describe StaticPagesController, :type => :controller do
     end
   end
 
+  context 'GET #contact' do
+    before do
+      get :contact
+    end
+
+    it 'responds successfully with an HTTP 200 status code' do
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
+  end
+
 end
