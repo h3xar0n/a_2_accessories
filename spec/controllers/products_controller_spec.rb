759158fa9980aba80ffc1f6faad83d3021a5c9a0
h3xar0n <aron@glambyadelina.com>
2017-04-08 10:37:56 -0400

    More coverage for other controllers

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -4,39 +4,39 @@ RSpec.describe ProductsController, type: :controller do
   let(:user) { FactoryGirl.create(:user) }
   let(:admin) { FactoryGirl.create(:admin) }
   let(:product) { FactoryGirl.create(:product) }
-    
-  describe "GET New Product" do
-    context "User is logged in" do
+  
+  describe 'GET new product' do
+    context 'Non-admin user is logged in' do
       before do
         sign_in user
       end
-      it "non-admin user cannot delete product" do
+      it 'non-admin user cannot delete product' do
         delete :destroy, params: {id: product.id}
         expect(response).to have_http_status(302)
       end
-      it "non-admin user cannot create product" do
+      it 'non-admin user cannot create product' do
         get :new
         expect(response).to have_http_status(302)
       end
-      it "non-admin user cannot update product" do
+      it 'non-admin user cannot update product' do
         get :edit, params: {id: product.id}
         expect(response).to have_http_status(302)
       end
     end
 
-    context "Admin is logged in" do
+    context 'Admin is logged in' do
       before do
         sign_in admin
       end
-      it "admin can delete product" do
+      it 'admin can delete product' do
         delete :destroy, params: {id: product.id}
         expect(response).to redirect_to(products_path)
       end
-      it "admin can create product" do
+      it 'admin can create product' do
         get :new
         expect(response).to have_http_status(200)
       end
-      it "admin user can update product" do
+      it 'admin can update product' do
         get :edit, params: {id: product.id}
         expect(response).to have_http_status(200)
       end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -7,18 +7,18 @@ describe UsersController, :type => :controller do
   end
 
   describe 'GET #show' do
-      context 'User is logged in' do
-        before do 
-          sign_in @user
-        end
-
-        it 'Loads correct user details' do 
-          get :show, id: @user.id
-          expect(response).to be_success
-          expect(response).to have_http_status(200)
-          expect(assigns(:user)).to eq @user
-        end
+    context 'User is logged in' do
+      before do 
+        sign_in @user
       end
+
+      it 'Loads correct user details' do 
+        get :show, id: @user.id
+        expect(response).to be_success
+        expect(response).to have_http_status(200)
+        expect(assigns(:user)).to eq @user
+      end
+    end
 
     context 'No user is logged in' do
         it 'Redirects to login' do
